8b2674537c9925e33eef38dbe25d2dc110ea63e9
shijiaxiongw <1043495113@qq.com>
2021-08-27 23:16:24 +0800

    JWTUtil

--- src/main/java/com/sz/ums/UmsApplication.java
+++ src/main/java/com/sz/ums/UmsApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan("com.sz.ums.filter")
 public class UmsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UmsApplication.class, args);
     }
-
 }
