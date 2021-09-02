7b4d44dd08bf19215a53211a5f11ef48b9913be9
shijiaxiongw <1043495113@qq.com>
2021-09-03 00:52:27 +0800

    Department

---
+++ src/main/java/com/sz/ums/domain/Department.java
@@ -0,0 +1,20 @@
+package com.sz.ums.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "ums_department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "code",length = 50)
+    private String code;
+    @Column(name = "name",length = 50)
+    private String name;
+    @OneToMany(fetch = FetchType.LAZY)
+    private Organization organization;
+}
